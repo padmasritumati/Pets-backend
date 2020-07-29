a105ec0de50aa7725f00fb95391ace07f5e5ce55
padmasritumati <tumatipadmasri98@gmail.com>
2020-07-29 11:38:48 +0200

    added seeds for user,address and services table

---
+++ seeders/20200729090440-some-address.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "addresses",
+      [
+        {
+          house_number: 420,
+          street: "zebraspoor",
+          city: "maarssen",
+          postcode:"3605GX",
+          country:"Netherlands",
+          userId:1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          house_number: 4,
+          street: "duivenkamp",
+          city: "maarssen",
+          postcode:"3607BK",
+          country:"Netherlands",
+          userId:2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("addresses", null, {});
+  }
+};
