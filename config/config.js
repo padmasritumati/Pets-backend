afa20eb5ad8c159bd4d210bd5410d9692e45cf9e
padmasritumati <tumatipadmasri98@gmail.com>
2020-08-10 04:50:31 +0200

    changes in config.js

--- config/config.js
+++ config/config.js
@@ -15,12 +15,7 @@ module.exports = {
     operatorsAliases: "0",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "postgres",
-    operatorsAliases: "0",
+    use_env_variable: DATABASE_URL_DEV
   },
   
 };
